b9700c95252799216abada94cc65b7ad84e7922a
Nicolas Marini <nmarini44@gmail.com>
2019-05-17 18:52:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ function sayHey() {
 function sayHeyFriend(name) {
   return `Hey, ${name}!`
 }
+
+function favoriteIceCream(icecream) {
+  return `I love ${icecream}`
+}
+
+function shouting(string) {
+  return string.toUpperCase()
+}
+
+function roundDown(number) {
+  return Math.round(number)
+}
